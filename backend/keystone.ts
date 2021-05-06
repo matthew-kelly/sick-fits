8d5f77a7a7f1815a3cb9e493d47bf937bfb9b5da
Matt Kelly <bmatthewkelly@gmail.com>
2021-05-05 18:33:16 -0700

    create User schema for Keystone

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/test';
 
@@ -22,6 +23,7 @@ export default config({
   },
   lists: createSchema({
     // TODO: add schema items
+    User,
   }),
   ui: {
     // TODO: change this for roles
