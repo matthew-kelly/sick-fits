188e08e6dbc70964d1f2f41496141743b92f39b7
Matt Kelly <bmatthewkelly@gmail.com>
2021-05-13 12:35:22 -0700

    add product search to front end

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import Cart from './Cart';
 import Nav from './Nav';
+import Search from './Search';
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -30,7 +31,7 @@ const HeaderStyles = styled.header`
   .sub-bar {
     border-bottom: 1px solid var(--black);
     display: grid;
-    grid-template-columns: auto 1fr;
+    grid-template-columns: 1fr auto;
   }
 `;
 
@@ -44,7 +45,7 @@ export default function Header() {
         <Nav />
       </div>
       <div className="sub-bar">
-        <p>Search</p>
+        <Search />
       </div>
       <Cart />
     </HeaderStyles>
