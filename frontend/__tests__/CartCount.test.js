40e5ced4194ff708e617a78b2f18644aef2a19e3
Matt Kelly <bmatthewkelly@gmail.com>
2021-11-23 14:12:48 -0800

    write CartCount tests

---
+++ frontend/__tests__/CartCount.test.js
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import wait from 'waait';
+import CartCount from '../components/CartCount';
+
+describe('<CartCount />', () => {
+  it('renders', () => {
+    render(<CartCount />);
+  });
+
+  it('matches snapshot', () => {
+    const { container } = render(<CartCount count={11} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('updates via props', async () => {
+    const { container, rerender, debug } = render(<CartCount count={11} />);
+    expect(container).toHaveTextContent('11');
+
+    rerender(<CartCount count={12} />);
+    expect(container).toHaveTextContent('1211'); // transition shows both temporarily
+    await wait(400);
+    expect(container).toHaveTextContent('12');
+    expect(container).toMatchSnapshot();
+  });
+});
